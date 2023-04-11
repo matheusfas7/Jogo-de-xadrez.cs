8bb50ed73cf610674bb9976a6b44604b9072ef75
Matheus Angeli <matheusfariaangelisilva@gmail.com>
2023-04-11 12:00:05 -0300

    Controlando as pecas capturadas

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -11,10 +11,7 @@ try
         try
         {
             Console.Clear();
-            Tela.imprimirTabuleiro(partida.Tab);
-            Console.WriteLine();
-            Console.WriteLine("Turno: " + partida.Turno);
-            Console.WriteLine("Aguardando jogada: " + partida.JogadorAtual);
+            Tela.imprimirPartida(partida);
 
             Console.WriteLine();
             Console.Write("Origem: ");
