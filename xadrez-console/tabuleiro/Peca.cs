94399b23aac9b7fe98de6b47aa9c1fafc5b41243
Matheus Angeli <matheusfariaangelisilva@gmail.com>
2023-04-18 17:05:47 -0300

    Implementando a verificacao de xeque

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -47,5 +47,10 @@ namespace tabuleiro
         {
             QteMovimentos++;
         }
+
+        public void DecrementarQteMovimentos()
+        {
+            QteMovimentos--;
+        }
     }
 }
